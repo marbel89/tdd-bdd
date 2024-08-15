84fef405af5bbe44c64a550f00da06a26caa1d47
marbel89 <markbellmann89@gmail.com>
2024-08-15 07:37:32 -0400

    model sad paths

--- service/models.py
+++ service/models.py
@@ -108,9 +108,13 @@ class Product(db.Model):
 
     def delete(self):
         """Removes a Product from the data store"""
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
+        try:
+            logger.info("Deleting %s", self.name)
+            db.session.delete(self)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return False
 
     def serialize(self) -> dict:
         """Serializes a Product into a dictionary"""
