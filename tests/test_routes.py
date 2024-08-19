9f27254762c265b0923c447a1e13c65da06ad540
marbel89 <markbellmann89@gmail.com>
2024-08-19 11:48:25 -0400

    missing test fix

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -200,6 +200,23 @@ class TestProductRoutes(TestCase):
         # assert that the len() of the data is 5 (the number of products you created)
         self.assertEqual(len(data), 5)
 
+    def test_query_by_category(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
+        category = products[0].category
+        found = [product for product in products if product.category == category]
+        found_count = len(found)
+        logging.debug("Found Products [%d] %s", found_count, found)
+
+        # test for available
+        response = self.client.get(BASE_URL, query_string=f"category={category.name}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), found_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["category"], category.name)
+            
     def test_query_by_name(self):
         """It should Query Products by name"""
         products = self._create_products(5)
